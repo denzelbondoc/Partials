7b133c9926660de78403e810cdbfacf3d67fee71
unknown <cit183g-2@M303.UC-BCF.EDU.PH>
2023-03-08 18:13:35 +0800

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+
+class UserController extends Controller
+{
+    //
+    public function register() 
+    {
+        return view('user.register');
+    }
+
+    public function store(Request $req)
+    {
+        $validated=$req->validate([
+            "name"=>['required' ,'min:4'],
+            "email"=>['required','email', Rule::unique('users','email')],
+            "password"=>'required|confirmed|min:6'
+            
+        ]);
+
+        $validate['password']=Hash::make($validated['password']);
+        $user=User::create($validated);
+
+        return redirect("/");
+    }
+}
